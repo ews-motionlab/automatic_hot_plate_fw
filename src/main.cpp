fa7614920386714c74707510b3eed29c637068d5
Raquena <raquenaengineering@gmail.com>
2019-11-13 21:41:03 +0100

    Add main.c as a template with comments

---
+++ src/main.cpp
@@ -0,0 +1,48 @@
+
+// REQUREMENTS: (MAY VARY) ///////////////////////////////////////////////////////////////////////////
+
+  // PERIPHERALS: 
+    // - i2c bus (ir remote temperature sensor)
+    // - SPI bus (thermocouple contact controller)
+    // - bluetooth (for wireless communication)
+    // - Serial(debugging purposes)
+
+  // FUNCTIONALITY:
+
+    // PID controller (necessary to control accuerately the plate temperature).
+
+
+// LIBRARIES ////////////////////////////////////////////////////////////////////////////////////////
+
+// GLOBAL //
+// libraries that are globally installed in the computer (mostly included with the language)
+#include <Arduino.h>
+
+// LOCAL //
+// this libraries are the ones at the lib folder, which are downloaded from github and so on
+
+// CUSTOM //
+// custom created libraries
+
+
+// GLOBAL VARIABLE DECLARATION ///////////////////////////////////////////////////////////////////////
+
+
+
+// FUNCTION DECLARATION //////////////////////////////////////////////////////////////////////////////
+// here we define our functions, so they can be used from the beginning of the code.
+
+
+// INTERRUPTS IF NEEDED //////////////////////////////////////////////////////////////////////////////
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
+
+// FUNCTION IMPLEMENTATION //////////////////////////////////////////////////////////////////////////
+// here we IMPLEMENT the body of our functions
