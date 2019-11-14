aa23430303f2ede52b45222a4303e5d48704970c
Raquena <raquenaengineering@gmail.com>
2019-11-14 23:28:30 +0100

    - Add functionality to the firmware, system stops when too high temperature is reached. - Create a function to initialize the required IO pins. - add a pin to control the heater.

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 // REQUREMENTS: (MAY VARY) ///////////////////////////////////////////////////////////////////////////
 
   // PERIPHERALS: 
+    // - IO pin, enables and disables heater
     // - i2c bus (ir remote temperature sensor)
     // - SPI bus (thermocouple contact controller)
     // - bluetooth (for wireless communication)
@@ -9,7 +10,9 @@
 
   // FUNCTIONALITY:
 
-    // PID controller (necessary to control accuerately the plate temperature).
+    // 1. Completely stop if a too high temperature is reached (safety feature)
+    // 2. simple ON on until reaching max operating temp and OFF until below min. operating temp.
+    // 3. PID controller (necessary to control accuerately the plate temperature).
 
 
 // LIBRARIES ////////////////////////////////////////////////////////////////////////////////////////
@@ -29,6 +32,10 @@
 
 const unsigned char SDA_PIN = 25;                       // pins where the i2c bus will be connected
 const unsigned char SCL_PIN = 26;
+const unsigned char HEATER_SSR_PIN = 14;                // pin to switch the heater on and off. 
+
+const float ABSOLUTE_MAX_TEMP = 70;                     // defines the absolute maximum safe temperature.
+
 
 Adafruit_MLX90614 irThermometer = Adafruit_MLX90614(MLX90614_I2CADDR);    // creating object ir thermometer (input device i2c addr.)
 
@@ -37,23 +44,37 @@ double temp;                                            // variable to store rea
 // FUNCTION DECLARATION //////////////////////////////////////////////////////////////////////////////
 // here we define our functions, so they can be used from the beginning of the code.
 
+void initIO(void);                                      // initializes the IO pins as required.
+
 
 // INTERRUPTS IF NEEDED //////////////////////////////////////////////////////////////////////////////
 
 void setup() {
   // put your setup code here, to run once:
+  initIO();                                             // initializes all required IO pins 
   Serial.begin(115200);                                 // starts serial communication
   Wire.begin(SDA_PIN,SCL_PIN);                          // starts I2C communication 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println("LOOP STARTED");                       // basic debugging print to ensure setup is finished.
+  Serial.println("LOOP STARTED");                             // basic debugging print to ensure setup is finished.
   temp = irThermometer.readObjectTempC();                     // reads object temperature in ºC.
   Serial.println(temp);
+  if(temp > ABSOLUTE_MAX_TEMP){                               // if we reach the maximum safe temperature, we block the system
+    Serial.println("HIGHEST SAFE TEMPERATURE REACHED !!!");
+    Serial.println("System is blocked and heater disconnected");
+    Serial.println("Reset or switch off/on to unblock");
+    while(true);                                              // blocks the system
+  }
   delay(100);
 }
 
 
 // FUNCTION IMPLEMENTATION //////////////////////////////////////////////////////////////////////////
 // here we IMPLEMENT the body of our functions
+
+void initIO(){
+  pinMode(HEATER_SSR_PIN, OUTPUT);
+  digitalWrite(HEATER_SSR_PIN, LOW);
+}
